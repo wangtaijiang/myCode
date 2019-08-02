cd8d856755ebdaaaace2980ec37ae2d0d26f8375
superH <1042086797@qq.com>
2019-08-02 13:47:59 +0800

    SpringBoot之邮件任务，邮件内容用html，附件邮件发送

--- springboot/springboot-task/src/test/java/com/example/task/SpringbootTaskApplicationTests.java
+++ springboot/springboot-task/src/test/java/com/example/task/SpringbootTaskApplicationTests.java
@@ -1,11 +1,17 @@
 package com.example.task;
 
+import java.io.File;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -25,6 +31,26 @@ public class SpringbootTaskApplicationTests {
         message.setFrom("1042086797@qq.com");
 
         mailSender.send(message);
+    }
+
+    @Test
+    public void test02() throws MessagingException {
+        //1、创建一个复杂的消息邮件
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage,true);
+
+        //邮件设置
+        helper.setSubject("通知-今晚开会");
+        helper.setText("<b style='color:red'>今天 7:30开会</b>",true);
+        helper.setTo("1587856557@qq.com");
+        helper.setFrom("1042086797@qq.com");
+
+        //上传文件
+        helper.addAttachment("a.jpg",new File("D:\\a.jpg"));
+        helper.addAttachment("b.jpg",new File("D:\\b.jpg"));
+
+        mailSender.send(mimeMessage);
+
     }
 
 }
