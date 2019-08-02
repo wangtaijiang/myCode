4b5a052a70c8596ea65a0ca0483a432b66549339
superH <1042086797@qq.com>
2019-08-02 13:39:06 +0800

    SpringBoot之邮件任务，简单邮件发送

--- springboot/springboot-task/src/test/java/com/example/task/SpringbootTaskApplicationTests.java
+++ springboot/springboot-task/src/test/java/com/example/task/SpringbootTaskApplicationTests.java
@@ -2,15 +2,29 @@ package com.example.task;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootTaskApplicationTests {
 
+    @Autowired
+    JavaMailSenderImpl mailSender;
     @Test
     public void contextLoads() {
+        SimpleMailMessage message = new SimpleMailMessage();
+        //邮件设置
+        message.setSubject("通知-今晚开会！");
+        message.setText("今晚7：30开会");
+
+        message.setTo("1587856557@qq.com");
+        message.setFrom("1042086797@qq.com");
+
+        mailSender.send(message);
     }
 
 }
